67e48007741227ca43e26ee493c0d2bb8ce75d95
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-18 16:22:53 +0530

    Refactor user route handlers and remove unused code

--- src/app.js
+++ src/app.js
@@ -3,16 +3,34 @@ const express = require('express');
 const app = express()
 const port = 7777
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+// example of GET request handler
+app.get('/user', (req, res) => {
+  res.send({name: "Anushka", age: 20, email: "anushka@example.com"})
+})
 
+// example of POST request handler
+app.post('/user', (req, res) => {
+  res.send("User created successfully")
+})
+
+// example of DELETE request handler
+app.delete('/user', (req, res) => {
+  res.send("User deleted successfully")
+})
+
+// example of PUT request handler
+app.put('/user', (req, res) => {
+  res.send("User updated successfully")
+})
+
+// example of PATCH request handler
+app.patch('/user', (req, res) => {
+  res.send("User updated successfully")
+})
+
+// order of the routes matter, so if you have a route that matches a specific path, it should be defined before a more general route. Otherwise, the general route will catch the request and the specific route will never be reached.
 app.use("/test", (req, res) => { // request handler
   res.send("Hello from the server");
-});
-
-app.use("/hello", (req, res) => { // request handler
-  res.send("Hello jiii");
 });
 
 app.listen(port, () => {
