21cd6773853b1fa2007165350d170cd1aa49490e
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-18 15:15:54 +0530

    Add server setup and scripts to package.json

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 // creating Express.js Server
+const express = require('express');
+const app = express()
+const port = 7777
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.use("/test", (req, res) => { // request handler
+  res.send("Hello from the server");
+});
+
+app.use("/hello", (req, res) => { // request handler
+  res.send("Hello jiii");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
