01c13cd8bd89a40bc4b8ea8e6ecf13c434036129
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-19 17:11:44 +0530

    Refactor user data retrieval endpoint and error handling

--- src/app.js
+++ src/app.js
@@ -1,26 +1,23 @@
 // creating Express.js Server
 const express = require('express');
 const app = express();
-const port = 7777
-const { adminAuth, userAuth } = require('./middlewares/auth');
+const port = 7777;
 
-app.use("/admin", adminAuth);
-
-app.post("/user/login", (req,res) => {
-    res.send("User logged in!!")
+app.get("/getUserData", (req, res) => {
+    try{
+        throw new Error("Error while fetching user data!!");
+    res.send("user data sent!!");
+    }catch(err){
+        res.status(500).send("Internal Server Error!!");
+    }
 });
 
-app.get("/user", userAuth, (req, res) => {
-    res.end("User data sent!!");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-    res.end("All data sent!!");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-    res.end("User deleted!!");
-});
+app.use("/", (err, req, res, next) => {
+    if(err){
+        // log your error
+        res.status(500).send("Internal Server Error!!");
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
