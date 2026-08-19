27dfe51e66dbed274563a8bd459317bce6cb730d
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-19 16:56:55 +0530

    Implement user and admin authentication middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 // creating Express.js Server
 const express = require('express');
-const app = express()
+const app = express();
 const port = 7777
+const { adminAuth, userAuth } = require('./middlewares/auth');
 
-// example of GET request handler
-app.get('/user/:userId/:name/:password', (req, res) => { // dynamic routing
-    console.log(req.params) // route parameters
-    console.log(req.query) // query parameters
-    res.send({ name: "Anushka", age: 20, email: "anushka@example.com" })
-})
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req,res) => {
+    res.send("User logged in!!")
+});
+
+app.get("/user", userAuth, (req, res) => {
+    res.end("User data sent!!");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+    res.end("All data sent!!");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+    res.end("User deleted!!");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
