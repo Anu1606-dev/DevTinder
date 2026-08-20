1e1291c1eb9d65912e864a3bd1d4048696af67d8
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-20 13:16:28 +0530

    Implement user signup endpoint and save user data to the database

--- src/app.js
+++ src/app.js
@@ -2,7 +2,24 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
+const User = require('./models/user');
 const port = 7777;
+
+app.post("/signup", async (req, res) => {
+    // creating a new user instance using the User model
+    const user = new User({
+        firstName: "Anu",
+        lastName: "Sarkar",
+        email: "example@gmail.com",
+        password: "123456",
+        age: 21,
+        gender: "Female"
+    });
+    // saving the user instance to the database
+    await user.save();
+    // sending a response back to the client
+    res.send("User created successfully!!");
+});
 
 //Best practice-> connect the database first before starting the application
 connectDB().then(() => { 
