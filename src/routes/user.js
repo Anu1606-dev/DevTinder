d56c5757cff548a6a6ebd26602f8bb910c377a1b
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-29 22:24:39 +0530

    Refactor connectionRequest model reference and enhance user router to fetch accepted connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const userRouter = express.Router();
 const {userAuth} = require('../middlewares/auth');
-const connectionRequest = require('../models/connectionRequest');
+const ConnectionRequest = require('../models/connectionRequest');
+
+const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "about", "skills"];
 
 // get all the pending connection requests for the logged in user
 userRouter.get("/user/requests/received", userAuth, async(req, res) => {
     try{
         const loggedInUser = req.user;
-        const connectionRequests = await connectionRequest.find({
+        const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested",
         }).populate("fromUserId", ["firstName", "LastName", "photoUrl", "about", "skills"]);
@@ -19,6 +21,24 @@ userRouter.get("/user/requests/received", userAuth, async(req, res) => {
 
     }catch(err){
         res.status(500).send("Error!!" + err.message);
+    }
+});
+
+userRouter.get("/user/connections", userAuth, async (req,res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id, status: "accepted" },
+                { toUserId: loggedInUser._id, status: "accepted" }
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA);
+
+        const data = connectionRequests.map((row) => row.fromUserId)
+        res.json({ data });
+
+    }catch(err) {
+        res.status(400).send({ message: "Error!!" + err.message})
     }
 });
 
