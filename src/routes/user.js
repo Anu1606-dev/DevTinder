1cb445563b80a877dfb0e149ae5c262a606512b0
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-30 21:19:34 +0530

    Refactor user routes for improved readability and enhance feed endpoint to exclude users with connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const userRouter = express.Router();
-const {userAuth} = require('../middlewares/auth');
+const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "about", "skills"];
 
 // get all the pending connection requests for the logged in user
-userRouter.get("/user/requests/received", userAuth, async(req, res) => {
-    try{
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
@@ -19,12 +20,12 @@ userRouter.get("/user/requests/received", userAuth, async(req, res) => {
             data: connectionRequests,
         });
 
-    }catch(err){
+    } catch (err) {
         res.status(500).send("Error!!" + err.message);
     }
 });
 
-userRouter.get("/user/connections", userAuth, async (req,res) => {
+userRouter.get("/user/connections", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
         const connectionRequests = await ConnectionRequest.find({
@@ -37,7 +38,7 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
         console.log(connectionRequests);
 
         const data = connectionRequests.map((row) => {
-            if(row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
                 return row.toUserId;
             }
             return row.fromUserId;
@@ -45,48 +46,44 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
 
         res.json({ connectionRequests });
 
-    }catch(err) {
-        res.status(400).send({ message: "Error!!" + err.message})
+    } catch (err) {
+        res.status(400).send({ message: "Error!!" + err.message })
     }
 });
 
-// get the feed - list of users excluding connections, ignored, and current user
 userRouter.get("/user/feed", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
-        const skip = (page - 1) * limit;
 
-        // Find all connection requests (sent and received)
         const connectionRequests = await ConnectionRequest.find({
             $or: [
                 { fromUserId: loggedInUser._id },
                 { toUserId: loggedInUser._id }
             ]
-        });
+        }).select("fromUserId  toUserId");
 
-        // Extract all user IDs that have connection requests
-        const excludedUserIds = new Set();
-        excludedUserIds.add(loggedInUser._id.toString());
+        const hideUsersFromFeed = new Set();
         connectionRequests.forEach((req) => {
-            excludedUserIds.add(req.fromUserId.toString());
-            excludedUserIds.add(req.toUserId.toString());
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
         });
 
-        // Get feed - users not in connection list
-        const feed = await require('../models/user').find({
-            _id: { $nin: Array.from(excludedUserIds) }
-        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(USER_SAFE_DATA);
 
         res.json({
             message: "Feed fetched successfully!!",
-            data: feed,
+            data: users,
         });
+
     } catch (err) {
         res.status(400).send({ message: "Error!!" + err.message });
     }
-});
+})
 
 module.exports = userRouter;
 
