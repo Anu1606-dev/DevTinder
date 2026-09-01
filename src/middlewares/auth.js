9339ece57c791c0ace8714b163f8bcf9603e9f47
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-09-01 12:39:07 +0530

    Refactor error handling in user authentication middleware to use consistent response format

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,7 +9,7 @@ const userAuth = async (req, res, next) => {
         const { token } = cookies;
         // validate the token
         if(!token) {
-            throw new Error("Invalid token!!");
+            return res.status(401).send("Please login!!");
         }
 
         // extracxted decoded object 
@@ -21,13 +21,13 @@ const userAuth = async (req, res, next) => {
         const user = await User.findById(_id);
 
         if (!user) {
-            throw new Error("User not found!!");
+            return res.status(401).send("User not found!!");
         }
 
         req.user = user;
         next();
     }catch(err) {
-        res.status(400).send("ERROR: " + err.message);
+        return res.status(400).send("ERROR: " + err.message);
     }
 };
 
